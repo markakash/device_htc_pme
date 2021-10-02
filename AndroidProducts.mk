aac5139c9c5b0c249d0ecb1fa25e0fed1dc022ac
Akash Srivastava <akashniki@gmail.com>
2021-10-02 16:04:53 +0000

    DNM: PE

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_pme.mk
+    $(LOCAL_DIR)/aosp_pme.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_pme-userdebug
+    aosp_pme-userdebug
--- device.mk
+++ device.mk
@@ -27,8 +27,7 @@ $(call inherit-product-if-exists, vendor/htc/pme/pme-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
